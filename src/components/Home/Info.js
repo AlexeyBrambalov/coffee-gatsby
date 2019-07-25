821aee3203711d2eb35442dffa79b1a980e6ab18
Alexey Brambalov <brambalov@gmail.com>
2019-07-25 23:05:00 +0100

    2тв commit

---
+++ src/components/Home/Info.js
@@ -0,0 +1,24 @@
+import React from "react"
+import { Link } from "gatsby"
+
+const Info = () => {
+  return (
+    <section className="py-5">
+      <div className="container">
+        <div className="row">
+          <div className="col-10 col-sm-8 mx-auto text-center">
+            <p className="lead text-muted mb-5">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo
+              temporibus, quod quos at aliquam maiores, quibusdam dolore sint
+              modi porro eligendi praesentium, neque asperiores ipsam.
+              Accusamus, iure distinctio? Vel nam quisquam officia quibusdam
+              perspiciatis eum quae laboriosam expedita dolor minima?
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Info
