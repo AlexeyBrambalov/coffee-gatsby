085d6a52d13eccc1207d2b7e5efb5b278091ffcb
Alexey Brambalov <brambalov@gmail.com>
2019-08-13 22:09:35 +0100

    final local save

--- src/components/Home/Info.js
+++ src/components/Home/Info.js
@@ -1,10 +1,12 @@
 import React from "react"
 import { Link } from "gatsby"
+import Title from "../Globals/Title"
 
 const Info = () => {
   return (
     <section className="py-5">
       <div className="container">
+        <Title title="our story"></Title>
         <div className="row">
           <div className="col-10 col-sm-8 mx-auto text-center">
             <p className="lead text-muted mb-5">
@@ -14,6 +16,11 @@ const Info = () => {
               Accusamus, iure distinctio? Vel nam quisquam officia quibusdam
               perspiciatis eum quae laboriosam expedita dolor minima?
             </p>
+            <Link to="/about/">
+              <button className="btn text-uppercase btn-yellow">
+                about page
+              </button>
+            </Link>
           </div>
         </div>
       </div>
---
+++ src/pages/about.js
@@ -0,0 +1,33 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import Background from "../components/Globals/Background"
+import Info from "../components/Home/Info"
+
+const AboutPage = ({ data }) => (
+  <Layout>
+    <SEO title="Home" />
+    <Background
+      img={data.img.childImageSharp.fluid}
+      title="about us"
+      styleClass="about-background"
+    />
+    <Info />
+  </Layout>
+)
+
+export const query = graphql`
+  {
+    img: file(relativePath: { eq: "about-background.jpeg" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+  }
+`
+
+export default AboutPage
